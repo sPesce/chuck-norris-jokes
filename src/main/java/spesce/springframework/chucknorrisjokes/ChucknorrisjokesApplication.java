e510aa276ca2ec1b8908cfd9c6cabd7f6951b7d0
Steve <stevepesce879@gmail.com>
2020-12-08 16:53:50 -0500

    remade project from spring inzr

---
+++ src/main/java/spesce/springframework/chucknorrisjokes/ChucknorrisjokesApplication.java
@@ -0,0 +1,13 @@
+package spesce.springframework.chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisjokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spesce/springframework/chucknorrisjokes/ChucknorrisjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package spesce.springframework.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisjokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
