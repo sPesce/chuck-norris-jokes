97bf478e87f8d5dd9d60a82cb2e1692e29084de5
Steve <stevepesce879@gmail.com>
2020-12-08 16:47:00 -0500

    initialized spring

---
+++ chuckjokes/src/main/java/spesce/springframework/chuckjokes/ChuckjokesApplication.java
@@ -0,0 +1,13 @@
+package spesce.springframework.chuckjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckjokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckjokesApplication.class, args);
+    }
+
+}
---
+++ chuckjokes/src/test/java/spesce/springframework/chuckjokes/ChuckjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package spesce.springframework.chuckjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckjokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
