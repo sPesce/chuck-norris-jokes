465732ac2a6325880f45cba41cdc195d6d623fa5
Steve <stevepesce879@gmail.com>
2020-12-22 16:21:02 -0500

    refactored -- made chuckNorrisQuotes() bean

---
+++ src/main/java/spesce/springframework/chucknorrisjokes/config/ChuckConfiguration.java
@@ -0,0 +1,20 @@
+package spesce.springframework.chucknorrisjokes.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created By Steve P. on 12/22/2020
+ * original package: spesce.springframework.chucknorrisjokes.config
+ * ------------------------------------------------------------------------------------------
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
